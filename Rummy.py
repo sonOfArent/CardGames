80dc91a258466752a8df7272953c2f7948e339c7
sonOfArent <91688309+sonOfArent@users.noreply.github.com>
2022-04-25 20:51:47 -0500

    Rummy Initialization

---
+++ Rummy.py
@@ -0,0 +1,7 @@
+from CardGame import CardGame
+from Player import Player
+from CustomFunctions import CustomFunctions
+
+
+# This is a complicated card game to recreate, as the game involves many separate groups of cards co-existing in many different ways. However, it shouldn't be too much more difficult than, say, Blackjack, it'll just take a bit longer to figure some things out.
+
